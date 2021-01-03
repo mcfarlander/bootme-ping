1b6e1e32a8eda102642c9148bbabfbf973e38218
David Johnson <mcfarlander@charter.net>
2021-01-03 09:33:07 -0600

    adding back in spring boot security, setting web config to allow most endpoints

--- src/main/java/org/djohnson/bootme/ping/jwt/JwtTokenUtil.java
+++ src/main/java/org/djohnson/bootme/ping/jwt/JwtTokenUtil.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
@@ -86,16 +86,16 @@ public class JwtTokenUtil implements Serializable {
 		return expiration.before(new Date());
 	}
 	
-//	/**
-//	 * Create a JWT for the user.
-//	 * 
-//	 * @param userDetails {@link UserDetails}
-//	 * @return JWT string
-//	 */
-//	public String generateToken(UserDetails userDetails) {
-//		Map<String, Object> claims = new HashMap<>();
-//		return doGenerateToken(claims, userDetails.getUsername());
-//	}
+	/**
+	 * Create a JWT for the user.
+	 * 
+	 * @param userDetails {@link UserDetails}
+	 * @return JWT string
+	 */
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		return doGenerateToken(claims, userDetails.getUsername());
+	}
 
 	//while creating the token -
 	//1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
@@ -109,15 +109,15 @@ public class JwtTokenUtil implements Serializable {
 				.signWith(SignatureAlgorithm.HS512, secret).compact();
 	}
 
-//	/**
-//	 * Validate the JWT based on the token and the user.
-//	 * @param token the JWT to use
-//	 * @param {@link UserDetails} the user's details
-//	 * @return true if the user information matches what is in the JWT, false otherwise
-//	 */
-//	public Boolean validateToken(String token, UserDetails userDetails) {
-//		final String username = getUsernameFromToken(token);
-//		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-//	}
+	/**
+	 * Validate the JWT based on the token and the user.
+	 * @param token the JWT to use
+	 * @param {@link UserDetails} the user's details
+	 * @return true if the user information matches what is in the JWT, false otherwise
+	 */
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = getUsernameFromToken(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
 
 }
